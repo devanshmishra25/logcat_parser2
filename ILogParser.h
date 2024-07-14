f860adb5bdaf505f77ea4722b6b5cf8900da26e6
devanshmishra25 <80719667+devanshmishra25@users.noreply.github.com>
2024-07-15 00:01:10 +0530

    iLogparser explanation comments

--- ILogParser.h
+++ ILogParser.h
@@ -7,8 +7,12 @@
  
 class ILogParser {
 public:
-    virtual ~ILogParser() = default;
+    virtual ~ILogParser() = default; //It's crucial for interfaces where inheritance is used to 
+                                     // ensure proper cleanup of derived class objects through base class pointers.
+
+
     virtual std::vector<LogEntry> parse(const std::string& filePath) = 0;
+    // This function is expected to take a file path as input and return a vector of LogEntry objects, representing the parsed log entries
 };
  
 #endif // ILOGPARSER_H
